f303ad6245878b95cc16888f61473be8e87a7998
chen <602042954@qq.com>
2023-07-25 15:19:36 +0800

    Initial commit

---
+++ provider/src/main/java/com/easy/andProvider/AndProviderApplication.java
@@ -0,0 +1,18 @@
+
+package com.easy.andProvider;
+
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.apache.dubbo.config.spring.context.annotation.EnableDubboConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableDubbo
+public class AndProviderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AndProviderApplication.class);
+    }
+}
